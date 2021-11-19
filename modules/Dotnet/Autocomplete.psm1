a936b48efcb4afbe068cf1e7092a230c9e83d896
Patrick Christensen <patrick.christensen@eksponent.com>
2021-11-19 15:13:54 +0100

    Remove puma-autocomplete as it isn't relevant here

--- modules/Dotnet/Autocomplete.psm1
+++ modules/Dotnet/Autocomplete.psm1
@@ -5,13 +5,3 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
-
-Register-ArgumentCompleter -Native -CommandName puma -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-
-    $command = $parameterName.CommandElements | Join-String -Property Value -Separator " "
-
-    puma complete $command | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-}
