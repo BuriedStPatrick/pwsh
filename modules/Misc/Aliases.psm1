6e64f998daffe928f494e1b006265569d99c1f95
Patrick Christensen <patrick.christensen@eksponent.com>
2021-11-29 15:49:46 +0100

    Write-WarningMessage when can't find command in PATH

--- modules/Misc/Aliases.psm1
+++ modules/Misc/Aliases.psm1
@@ -1,7 +1,14 @@
 function Invoke-WhereIs {
     $ErrorActionPreference = "SilentlyContinue"
-    return (Get-Command @args)?.Path
+    $path = (Get-Command @args)?.Path
+
+    if ($null -eq $path) {
+        Write-WarningMessage "Not found: $($args)"
+    }
+
+    return $path
 }
 
 Set-Alias whereis Invoke-WhereIs
 Set-Alias open Invoke-Item
+Set-Alias json ConvertTo-Json
